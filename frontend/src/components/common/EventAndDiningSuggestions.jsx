07032945c7fe78f91fc51b15e1bda01d756604a3
nikhilrana13 <nikhilrajput060@gmail.com>
2026-03-05 11:03:53 +0530

    Added onClick event to SearchResultCard link and useDialog hook

--- frontend/src/components/common/EventAndDiningSuggestions.jsx
+++ frontend/src/components/common/EventAndDiningSuggestions.jsx
@@ -79,7 +79,6 @@ const EventAndDiningSuggestions = () => {
                         )
                         }
                         </div>
-                       
                     </div>
                 </div>
         </>
--- frontend/src/components/common/SearchResultCard.jsx
+++ frontend/src/components/common/SearchResultCard.jsx
@@ -1,15 +1,16 @@
+import { useDialog } from "../../context/useDialog"
 import { Link } from "react-router-dom"
 import slugify from "slugify"
 
 const SearchResultCard = ({ item }) => {
-
+  const {setIsEventAndDiningOpen} = useDialog()
   const safeSlug = (value) => slugify(value || "", { lower: true, strict: true })
   
    const eventUrl = `/events/${item?._id}/${safeSlug(item?.title)}`
    const diningUrl = `/dining/${safeSlug(item?.city)}/${item?._id}/${safeSlug(item?.name)}`
 
   return (
-    <Link to={item.type === "event" ? eventUrl : diningUrl}>
+    <Link to={item.type === "event" ? eventUrl : diningUrl} onClick={()=>setIsEventAndDiningOpen(false)}>
       <div className="flex items-start gap-4 p-3 rounded-xl hover:bg-gray-100 transition-all duration-200 cursor-pointer">
       {/* Image */}
       <div className="w-16 h-16 rounded-xl overflow-hidden flex-shrink-0">
