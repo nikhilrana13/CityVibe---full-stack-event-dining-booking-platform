e2997a301a3f8132b6ec575ee9e4c3e680d60129
nikhilrana13 <nikhilrajput060@gmail.com>
2026-06-21 17:58:30 +0530

    fix: Add spacing before login dialog in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,6 +48,7 @@ const App = () => {
         {/* for organizers */}
         <Route path="/events/list-your-events" element={<ListYourEvents />} />
       </Routes>
+      
       {/* login dialog */}
       <LoginDialog />
       <Toaster />
